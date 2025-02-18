3787ffacf5b185b78a639c94ab1f8ed655233d3d
jandre3397 <jael.andre97@gmail.com>
2025-02-18 14:07:34 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,16 @@
+const fs = require('fs');
+
+// ✅ Read trips.json synchronously
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { 
+        title: 'Travlr Getaways',
+        trips  // ✅ Pass trip data to the template
+    });
 };
 
 module.exports = {
     travel
 };
-
